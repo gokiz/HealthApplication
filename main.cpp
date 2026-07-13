d5fbd083a0279997905edfb0e7720f37466e1943
Gökçe <ozturkgokceyeliz@gmail.com>
2026-07-13 13:30:40 +0300

    13.07.26(3):Butona tiklama fonksiyonu ve mesaj kutusu eklendi.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <QApplication>
 #include <QWidget>
 #include <QPushButton>
+#include <QMessageBox>
 
 int main(int argc, char *argv[])
 {
@@ -9,11 +10,15 @@ int main(int argc, char *argv[])
 
     // Temel boş bir pencere (Widget) nesnesi oluşturuyoruz
     QWidget anaPencere;
-    anaPencere.setWindowTitle("Butonlu Pencere");
+    anaPencere.setWindowTitle("Interactive Window");
     anaPencere.resize(400, 300); // Genişlik: 400, Yükseklik: 300
-    QPushButton *button = new QPushButton("tıkla", &anaPencere);
-
+    QPushButton *button = new QPushButton("Click Here", &anaPencere);
     button->setGeometry(150, 130, 100, 40);
+
+
+    QObject::connect(button, &QPushButton:: clicked, &anaPencere, [&anaPencere](){
+        QMessageBox::information(&anaPencere, "Information", "You have taken the first step towards the health app.");
+    });
     // Pencereyi ekranda gösterir
     anaPencere.show();
 
