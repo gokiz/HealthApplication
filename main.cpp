972f7fcef48b6215b93a5ce3aeac0df5126e28e6
Gökçe <ozturkgokceyeliz@gmail.com>
2026-07-13 13:20:19 +0300

    2.adım:pencereye buton eklendi: 13.07.26

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QWidget>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
@@ -8,9 +9,11 @@ int main(int argc, char *argv[])
 
     // Temel boş bir pencere (Widget) nesnesi oluşturuyoruz
     QWidget anaPencere;
-    anaPencere.setWindowTitle("Boş Pencere");
+    anaPencere.setWindowTitle("Butonlu Pencere");
     anaPencere.resize(400, 300); // Genişlik: 400, Yükseklik: 300
+    QPushButton *button = new QPushButton("tıkla", &anaPencere);
 
+    button->setGeometry(150, 130, 100, 40);
     // Pencereyi ekranda gösterir
     anaPencere.show();
 
