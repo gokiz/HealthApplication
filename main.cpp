4a134fd6b7d673448713b3aeca67823f8c9ad90d
Gökçe <ozturkgokceyeliz@gmail.com>
2026-07-13 13:57:28 +0300

    13.07.26(5): Boy girisi eklendi.

--- main.cpp
+++ main.cpp
@@ -12,30 +12,38 @@ int main(int argc, char *argv[])
 
     // Temel boş bir pencere (Widget) nesnesi oluşturuyoruz
     QWidget anaPencere;
-    anaPencere.setWindowTitle("Health Application-Login");
-    anaPencere.resize(400, 300); // Genişlik: 400, Yükseklik: 300
+    anaPencere.setWindowTitle("Health Application-Weight & Height");
+    anaPencere.resize(400, 350); // Genişlik: 400, Yükseklik: 300
 
 
     QLabel *kiloEtiketi = new QLabel("Weight(kg): ", &anaPencere);
-    kiloEtiketi->setGeometry(50,80,100,30);
+    kiloEtiketi->setGeometry(50,60,100,30);
 
 
     QLineEdit *kiloGiris = new QLineEdit(&anaPencere);
-    kiloGiris->setGeometry(160,80,150,30);
+    kiloGiris->setGeometry(160,60,150,30);
     kiloGiris->setPlaceholderText("Example: 70");
 
+    QLabel *boyEtiketi = new QLabel("Weight: ", &anaPencere);
+    boyEtiketi->setGeometry(50,110,100,30);
+
+    QLineEdit *boyGiris = new QLineEdit(&anaPencere);
+    boyGiris->setGeometry(160,110,150,30);
+    boyGiris->setPlaceholderText("Example:175");
 
     QPushButton *button = new QPushButton("Save", &anaPencere);
-    button->setGeometry(150, 130, 100, 40);
+    button->setGeometry(160, 180, 150, 40);
 
     // Butona tıklandığında girilen kiloyu mesaj olarak gösterelim
-    QObject::connect(button, &QPushButton::clicked, &anaPencere, [kiloGiris, &anaPencere]() {
-        QString girilenKilo = kiloGiris->text(); // Kutudaki yazıyı alıyoruz
+    QObject::connect(button, &QPushButton::clicked, &anaPencere, [kiloGiris, boyGiris, &anaPencere]() {
+        QString kilo = kiloGiris->text();
+        QString boy = boyGiris->text();
 
-        if(girilenKilo.isEmpty()) {
-            QMessageBox::warning(&anaPencere, "Warning", "Enter your weight!");
+        if(kilo.isEmpty() || boy.isEmpty()) {
+            QMessageBox::warning(&anaPencere, "Warning", "Please fill in all the fields!");
         } else {
-            QMessageBox::information(&anaPencere, "Successful", "Weigh was recorded: " + girilenKilo + " kg");
+            QMessageBox::information(&anaPencere, "Successful",
+                                     "data was received!\nWeight: " + kilo + " kg\nHeight: " + boy + " cm");
         }
     });
 
