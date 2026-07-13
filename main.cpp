c6950154cc5fda52bb518552c48dcec940f69bdc
Gökçe <ozturkgokceyeliz@gmail.com>
2026-07-13 13:44:21 +0300

    13.07.26(4):Kilo giris kutusu ve etiketi eklendi.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <QApplication>
 #include <QWidget>
 #include <QPushButton>
+#include <QLabel>
+#include <QLineEdit>
 #include <QMessageBox>
 
 int main(int argc, char *argv[])
@@ -10,15 +12,33 @@ int main(int argc, char *argv[])
 
     // Temel boş bir pencere (Widget) nesnesi oluşturuyoruz
     QWidget anaPencere;
-    anaPencere.setWindowTitle("Interactive Window");
+    anaPencere.setWindowTitle("Health Application-Login");
     anaPencere.resize(400, 300); // Genişlik: 400, Yükseklik: 300
-    QPushButton *button = new QPushButton("Click Here", &anaPencere);
+
+
+    QLabel *kiloEtiketi = new QLabel("Weight(kg): ", &anaPencere);
+    kiloEtiketi->setGeometry(50,80,100,30);
+
+
+    QLineEdit *kiloGiris = new QLineEdit(&anaPencere);
+    kiloGiris->setGeometry(160,80,150,30);
+    kiloGiris->setPlaceholderText("Example: 70");
+
+
+    QPushButton *button = new QPushButton("Save", &anaPencere);
     button->setGeometry(150, 130, 100, 40);
 
+    // Butona tıklandığında girilen kiloyu mesaj olarak gösterelim
+    QObject::connect(button, &QPushButton::clicked, &anaPencere, [kiloGiris, &anaPencere]() {
+        QString girilenKilo = kiloGiris->text(); // Kutudaki yazıyı alıyoruz
 
-    QObject::connect(button, &QPushButton:: clicked, &anaPencere, [&anaPencere](){
-        QMessageBox::information(&anaPencere, "Information", "You have taken the first step towards the health app.");
+        if(girilenKilo.isEmpty()) {
+            QMessageBox::warning(&anaPencere, "Warning", "Enter your weight!");
+        } else {
+            QMessageBox::information(&anaPencere, "Successful", "Weigh was recorded: " + girilenKilo + " kg");
+        }
     });
+
     // Pencereyi ekranda gösterir
     anaPencere.show();
 
